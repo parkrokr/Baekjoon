546151c36fb98e50cd49ddbab9d6f56465ca3be9
parkrokr <parkrokr@gmail.com>
2020-12-05 18:06:18 +0900

    study code

---
+++ Lv_3/10871.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,x,a;
+	
+	cin >> n >> x;
+	
+	for (int i(0); i < n; i++)
+	{
+		cin >> a;
+		if (a < x)
+		{
+			cout << a << " ";
+		}
+	}
+	cout << endl;
+
+	return 0;
+}
